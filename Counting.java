7c32ff81964bd50e3ab2c517f1f6093b3a0e6ea8
RacquelNinaDennison <racquelnina@gmail.com>
2023-10-17 14:50:51 +0200

    Added some problem descriptions

--- Counting.java
+++ Counting.java
@@ -1,12 +1,73 @@
+
+/**
+ * Problem Description
+Consider the following pseudo-code function:
+int unknown(int n) {
+if (n≤0) {
+print 0;
+return 1;
+}
+else if (n==1) {
+print 1;
+return 1;
+}
+else {
+return unknown(n-1)*unknown(n-3);
+}
+}
+Given a single integer, Ni, what is the number, pi, of zeroes, and the number, qi, of ones, that will be
+printed by a call to unknown with Ni as the parameter value, i.e., the number of '0's and the number
+of '1's printed by the call unknown(Ni).
+Example
+Let Ni=3, the steps in the computation of unknown(3) are as follows:
+unknown(3) becomes unknown(2)*unknown(0)
+unknown(2) becomes unknown(1)*unknown(-1)
+unknown(1) becomes print 1
+unknown(-1) becomes print 0
+unknown(0) becomes print 0
+So, for Ni=3, the number of zeroes, pi, is 2, and the number of ones, qi, is 1.
+Hints
+You should use Java type 'long' or C++/C type 'long long' for pi and qi.
+Input and Output
+Program input and output will make use of stdio streams (System.in and System.out in Java)
+i.e., not file I/O.
+The first line of input consists of a value M, the number of cases for which the problem must be
+solved, the next M lines will each contain a value for one of those cases i.e., for N0, N1, …, NM-1.
+3
+Continued
+Sample Input:
+4
+3
+5
+10
+0
+Output consists of exactly M lines, one for each Ni. Each will contain exactly two space separated
+integers, pi and qi, followed by a line break — in Java, for example, use System.out.println,
+not System.out.print. The automatic marker expects this precise form.
+Sample Output
+2 1
+3 3
+22 19
+1 0
+Constraints
+1 ≤ M ≤ 101, in 50% of test cases, M≤5.
+0 ≤ Ni ≤ 100
+Scoring
+Each test case that is answered correctly will score 10 points.
+
+ * 
+ * 
+ */
+
 import java.util.Scanner;
 
 public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int M = scanner.nextInt(); 
+        int M = scanner.nextInt();
 
         for (int i = 0; i < M; i++) {
-            int N = scanner.nextInt(); 
+            int N = scanner.nextInt();
             Pair result = countZerosOnes(N);
             System.out.println(result.p + " " + result.q);
         }
--- Path.java
+++ Path.java
@@ -1,5 +1,60 @@
+
+/**
+ * Problem Description
+Write a program that computes the number of paths that a robot may take when navigating through a given terrain.
+
+A terrain is represented by an NxN grid of squares.
+The robot starts in the lower left square of grid, referred to as square (1, 1), and needs to move to the upper right square, referred to as (N, N).
+A terrain also contains K obstacles, each of which blocks a single square on the grid.
+The robot cannot move into a square containing an obstacle, so these reduce the number of possible paths through the grid.
+No two obstacles block the same square, and the starting and ending squares will never contain obstacles.
+The robot can only move a single square up or right with each step.
+Example
+Given a 3×3 grid with no obstacles, there are a total of 6 paths from the bottom left square to the top right, shown below:
+
+
+
+Given the same grid with one obstacle on square (2, 2) there are 2 paths:
+
+
+
+Given the same grid with one obstacle on square (2, 1) there are 3 paths:
+
+
+
+Input and Output
+Program input and output will make use of stdio streams (System.in and System.out in Java) i.e., not file I/O.
+
+Input consists of a series of lines, each containing up to two integer values.
+
+The first line of input contains a single integer, N, representing the size of the grid.
+The second line contains a single integer K, representing the number of obstacles on the grid.
+The following K lines of input each contain two integers separated by a space. Each of these lines represents the coordinates on the grid of one of the obstacles.
+Output consists of a single integer P, the number of paths from the bottom left grid square to the top right grid square,followed by a line break — in Java, for example, use System.out.println, not System.out.print. The automatic marker expects this precise form.
+
+Sample Input:
+
+3
+1
+2 1
+
+Sample output:
+
+3
+
+Constraints
+1 < N < 20 0 < K < N
+
+All obstacles will have coordinates within the dimensions of the grid.
+
+The answer, P, will be bounded by 0 ≤ P ≤ 1,000,000,000
+
+Scoring
+Each test case that is answered correctly will score 5 points.
+ */
+
 import java.util.HashMap;
-import java.util.Map;
+
 import java.util.Scanner;
 
 public class Path {
